aecbdfd4ef737e91da054e8ab54e6edcb2f77950
songsam <songsaem33@gmail.com>
2026-06-02 18:40:56 +0900

    project

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = "songman_secret_key"
 
+cart = []
 
 # DB 생성
 def create_table():
@@ -117,15 +118,137 @@ def pants():
     return render_template("pants.html")
 
 
-@app.route("/detail")
-def detail():
-    return render_template("detail.html")
-
-
 @app.route("/pay")
 def pay():
     return render_template("pay.html")
 
+
+@app.route("/detail/<int:product_id>")
+def detail(product_id):
+
+
+    products = {
+        1: {
+            "name": "오버핏 반팔 셔츠",
+            "price": "49,000원",
+            "image": "img1.jpg"
+        },
+
+        2: {
+            "name": "와이드 데님 팬츠",
+            "price": "59,000원",
+            "image": "img2.jpg"
+        },
+
+        3: {
+            "name": "미니멀 블레이저",
+            "price": "89,000원",
+            "image": "img3.jpg"
+        },
+        4: {
+            "name": "오버핏 셔츠",
+            "price": "39,000원",
+            "image": "img4.jpg"
+        },
+
+        5: {
+            "name": "미니멀 니트",
+            "price": "49,000원",
+            "image": "img5.jpg"
+        },
+
+        6: {
+            "name": "반팔 티셔츠",
+            "price": "29,000원",
+            "image": "img6.jpg"
+        },
+        7: {
+            "name": "와이드 슬랙스",
+            "price": "49,000원",
+            "image": "img7.jpg"
+        },
+
+        8: {
+            "name": "데님 팬츠",
+            "price": "59,000원",
+            "image": "img8.jpg"
+        },
+
+        9: {
+            "name": "조거 팬츠츠",
+            "price": "35,000원",
+            "image": "img9.jpg"
+        }       
+                    }
+
+    product = products[product_id]
+
+    return render_template(
+        "detail.html",
+        product=product,
+        product_id=product_id
+    )
+
+
+@app.route("/add_cart/<int:product_id>")
+def add_cart(product_id):
+
+    products = {
+        1: {
+            "name": "오버핏 반팔 셔츠",
+            "price": "49,000원",
+        },
+
+        2: {
+            "name": "와이드 데님 팬츠",
+            "price": "59,000원",
+        },
+
+        3: {
+            "name": "미니멀 블레이저",
+            "price": "89,000원",
+        },
+        4: {
+            "name": "오버핏 셔츠",
+            "price": "39,000원",
+        },
+
+        5: {
+            "name": "미니멀 니트",
+            "price": "49,000원",
+        },
+
+        6: {
+            "name": "반팔 티셔츠",
+            "price": "29,000원",
+        },
+        7: {
+            "name": "와이드 슬랙스",
+            "price": "49,000원",
+        },
+
+        8: {
+            "name": "데님 팬츠",
+            "price": "59,000원",
+        },
+
+        9: {
+            "name": "조거 팬츠츠",
+            "price": "35,000원",
+        }       
+    }
+
+    cart.append(products[product_id])
+
+    return redirect("/cart")
+
+@app.route("/cart")
+def cart_page():
+
+    return render_template(
+        "cart.html",
+        cart=cart
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
